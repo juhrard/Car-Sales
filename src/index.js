7e6515f6d3cead179a83eb781f0382e3288b5da4
Gerard Crockenberg <gmahred@gmail.com>
2020-01-22 18:15:02 -0500

    finished base project

--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { carReducer } from './reducers/carReducer';
+
+const store = createStore(carReducer);
+console.log(store.getState());
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+  );
